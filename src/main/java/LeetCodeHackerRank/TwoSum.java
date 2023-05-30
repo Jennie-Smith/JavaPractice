2fb28e499bf6fde5ce786430aa27ae67ce2b1554
Jennie Smith <Jsmith9spiral@gmail.com>
2023-05-30 17:07:33 -0500

    Best Time to buy and sell stock

--- src/main/java/LeetCodeHackerRank/TwoSum.java
+++ src/main/java/LeetCodeHackerRank/TwoSum.java
@@ -1,6 +1,34 @@
+package LeetCodeHackerRank;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return nums;
     }
+
+//    hashmap method - faster method - more memory
+    public int[] twoSumHashMap(int[] nums, int target){
+        Map<Integer, Integer> complements = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer complementIndex = complements.get(nums[i]);
+            if(complementIndex != null){
+                return new int[]{i, complementIndex};
+            }
+            complements.put(target - nums[i], i);
+        }
+        return nums;
+    }
+    
+//    {5, 2, 4}  = 6 ; answer = [1,2] or [2, 1]
+//    gets the index of the two numbers that equal the target
 }
