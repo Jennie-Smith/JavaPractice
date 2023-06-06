9460433cf3c1cdfe1385425388e8ff57fb9b6ccf
Jennie Smith <Jsmith9spiral@gmail.com>
2023-06-05 23:48:22 -0500

    more practice

--- src/main/java/LeetCodeHackerRank/ArrayExceptSelf.java
+++ src/main/java/LeetCodeHackerRank/ArrayExceptSelf.java
@@ -17,12 +17,30 @@ public class ArrayExceptSelf {
             left_products[i] = nums[i-1] * left_products[i-1];
         }
 
-        for (int i = N-2; i >=0 ; i--) {
+        for (int i = N-2; i >=0; i--) {
             right_products[i] = nums[i+1] * right_products[i+1];
         }
 
         for (int i = 0; i < N; i++) {
             output_arr[i] = left_products[i] * right_products[i];
+        }
+
+        return output_arr;
+    }
+
+//    or
+    public int[] productExceptSelf2(int[] nums) {
+        int N = nums.length;
+
+        int[] output_arr = new int[N];
+
+        for (int i = 1; i < N; i++) {
+            output_arr[i] = nums[i-1] * output_arr[i-1];
+        }
+        int R = 1;
+        for (int i = N-2; i >=0; i--) {
+            output_arr[i] = output_arr[i+1] * R;
+            R = R * nums[i];
         }
 
         return output_arr;
