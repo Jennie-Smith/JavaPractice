72b541b2bb95a9ea961b06aec5842a03786e4f46
Jennie Smith <Jsmith9spiral@gmail.com>
2023-06-13 22:24:47 -0500

    simple array sum

---
+++ src/main/java/LeetCodeHackerRank/SimpleArraySum.java
@@ -0,0 +1,15 @@
+package LeetCodeHackerRank;
+
+import java.util.List;
+
+public class SimpleArraySum {
+
+    public static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            sum = sum + ar.get(i);
+        }
+        return sum;
+
+    }
+}
