6eeeaaa6db1d39c7830ea698eb542680a32f14a8
Jennie Smith <Jsmith9spiral@gmail.com>
2023-07-07 15:06:01 -0500

    breaking the records

---
+++ src/main/java/LeetCodeHackerRank/BreakingTheRecords.java
@@ -0,0 +1,31 @@
+package LeetCodeHackerRank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BreakingTheRecords {
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        // Write your code here
+        int max_count = 0;
+        int min_count = 0;
+        int max_value = scores.get(0);
+        int min_value = scores.get(0);
+        for (int i = 0; i < scores.size(); i++) {
+            int val = scores.get(i);
+            if(val > max_value){
+                max_value = val;
+                max_count++;
+            }
+            if(val < min_value){
+                min_value = val;
+                min_count++;
+            }
+        }
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(max_count);
+        al.add(min_count);
+        return al;
+
+    }
+
+}
