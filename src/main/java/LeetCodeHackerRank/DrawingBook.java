caaa5401c13a2e50e1a4389960bec1a799d88b70
Jennie Smith <Jsmith9spiral@gmail.com>
2023-07-17 21:31:41 -0500

    drawing book

---
+++ src/main/java/LeetCodeHackerRank/DrawingBook.java
@@ -0,0 +1,9 @@
+package LeetCodeHackerRank;
+
+public class DrawingBook {
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        return Math.min(p/2, (n/2)-(p/2));
+
+    }
+}
